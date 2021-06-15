9ac087bc2484f06a2be30249d0645680415e1cac
871234342 <74284329+871234342@users.noreply.github.com>
2021-06-15 16:42:23 +0800

    Add rotate90

--- util.py
+++ util.py
@@ -2,13 +2,44 @@ import torch
 import pandas as pd
 import os
 import copy
+import random
 import numpy as np
 from skimage import io
 from PIL import Image
 from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
 import time
 
 
+class Rotate90:
+    '''
+    Rotate an image by 90, 180 or 270 degree
+    '''
+    def __init__(self, p=None):
+        '''
+        :param p: List of prob. of each rotation
+                  e.g. [0.1, 0.2, 0.3, 0.4]
+        '''
+        if p:
+            self.p = [0, 0, 0, 0]
+            for i in range(4):
+                for j in range(i + 1):
+                    self.p[i] += p[j]
+        else:
+            self.p = [0.25, 0.5, 0.75, 1]
+
+    def __call__(self, x):
+        choice = random.uniform(0, 1)
+        if choice < self.p[0]:
+            return x
+        elif choice < self.p[1]:
+            return TF.rotate(x, 90)
+        elif choice < self.p[2]:
+            return TF.rotate(x, 180)
+        elif choice < self.p[3]:
+            return TF.rotate(x, 270)
+
+            
 class DataWithLabel(Dataset):
     def __init__(self, annot, img_dir, transforms=None):
         '''
